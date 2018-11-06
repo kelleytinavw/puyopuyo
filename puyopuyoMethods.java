d532dad8352fd3d7d4b72f5a3b5807b7d78bdca6
kelleytinavw <kelleytina.williams@my.hamptonu.edu>
2018-11-06 10:30:04 -0500

    edited  methods

--- puyopuyoMethods.java
+++ puyopuyoMethods.java
@@ -1,3 +1,22 @@
 public class puyopuyoMethods{
+   private int SIZE = 10;
+   private int [][] boardStack = new int [SIZE][SIZE];
+   private int [] stack = new int [SIZE];
+   private int [] queue = new int [SIZE];
+   private int stack_top = 0;
+   private int frontQ = 0;
+   private int rearQ = 0;
+   private int count = 0;
    
+   public void storeNum(){
+      for(int i = 0; i < SIZE; i++){
+         stack[i] = i+2;
+         stack[i+1] = stack[i];
+         stack_top++;  
+      }
+   }
+   
+   public void createBorard(){
+      
+   }
 }
