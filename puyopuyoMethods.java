87d3bce125616092f68a1388a5d59fce32ed1870
adrianTJenkins <adrian.jenkins99@gmail.com>
2018-11-07 16:20:24 -0500

    made SIZE constant variable

--- puyopuyoMethods.java
+++ puyopuyoMethods.java
@@ -1,5 +1,5 @@
 public class puyopuyoMethods{
-   private int SIZE = 10;
+   private final int SIZE = 10;
    private int [][] boardStack = new int [SIZE][SIZE];
    private int [] stack = new int [SIZE];
    private int [] queue = new int [SIZE];
@@ -7,32 +7,33 @@ public class puyopuyoMethods{
    private int frontQ = 0;
    private int rearQ = 0;
    private int count = 0;
-   
+
    public void storeNum(int x){
       for(int i = 0; i < SIZE; i++){
          stack[i] = i+2;
          stack[i+1] = stack[i];
-         stack_top++;  
+         stack_top++;
       }
    }
-   
+
 //    public void createBoard(){
-//       
+//
 //    }
-   
+
    public void moveToBoard(){
       int positionX = 0;
       int positionY = 0;
        for(int i = stack_top -1; i >= 0; i--){
           for(int j = stack_top -1; j >= 0; j++){
-               
+
                //error
                //boardStack[stack[i]][stack[j-2]];
+
                stack_top-= 2;
-               
+
           }
-       } 
-   } 
-   
-   
-} 
+       }
+   }
+
+
+}
