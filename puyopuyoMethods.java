ff9079c221df1b666503a5f9eaf02723faf051ec
kelleytinavw <kelleytina.williams@my.hamptonu.edu>
2018-11-07 16:02:16 -0500

    edit methds

--- puyopuyoMethods.java
+++ puyopuyoMethods.java
@@ -19,4 +19,20 @@ public class puyopuyoMethods{
    public void createBoard(){
       
    }
+   
+   public void moveToBoard(){
+      int positionX = 0;
+      int positionY = 0;
+       for(int i = stack_top -1; i >= 0; i--){
+          for(int j = stack_top -1; j >= 0; j++){
+               
+               boardStack[positionX][positionY] = stack[i] stack[j-2];
+               temp_top_KW++;
+               stack_top_KW--;
+               
+          }
+       } 
+   } 
+   
+   
 }
