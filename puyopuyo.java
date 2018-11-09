9706b942e1d8e1c0fa00880ed8ae3c684e93c36a
kelleytinavw <kelleytina.williams@my.hamptonu.edu>
2018-11-08 20:12:35 -0600

    edit method and driver

--- puyopuyo.java
+++ puyopuyo.java
@@ -1,5 +1,8 @@
 public class puyopuyo{
    public static void main(String [] args){
-
+      puyopuyoMethods test = new puyopuyoMethods();
+      
+      test.storeNum();
+      test.moveToBoard();
    }
 }
--- puyopuyoMethods.java
+++ puyopuyoMethods.java
@@ -12,7 +12,9 @@ public class puyopuyoMethods{
    private int rearQ = 0;
    private int count = 0;
    Scanner kb = new Scanner(System.in);
-
+   
+   public puyopuyoMethods(){}
+   
    public void storeNum(){
       int num = 0;
       System.out.println("Enter 20 numbers ranging from 1-9:");
@@ -26,18 +28,18 @@ public class puyopuyoMethods{
          stack_top++;
       }
    }
-
-//    public void createBoard(){
-//
-//    }
-
+ 
    public void moveToBoard(){
       int positionX = 0;
       int positionY = 0;
        for(int i = stack_top -1; i >= 0; i--){
           for(int j = stack_top -1; j >= 0; j++){
                boardStack[positionX][positionY] = stack[i];
-               boardStack[positionY][positionX] = stack[j-2];
+               boardStack[positionY][positionX] = stack[j-1];
+               //having error message here trying to assign the values into the board
+              //arrayIndexOutOfBoundsExeception: 10 
+              //at puyopuyoMethods.moveToBoard(puyopuyoMethods.java:41)
+              //at puyopuyo.main(puyopuyo.java:6)
                if(boardStack[positionX][positionY] == boardStack[positionY][positionX]){
                   queue[rearQ] = boardStack[positionX][positionY];
                   rearQ++;
@@ -45,8 +47,10 @@ public class puyopuyoMethods{
                   rearQ++;
                   
                   positionX++;
-                  positionY++;      
-                  stack_top-= 2;
+                  positionY++;
+                  if(stack_top != 1){
+                     stack_top -= 2;
+                  }     
                }else{
                   positionX++;
                   positionY++;      
