dc41e9d91e99a5815dfe86b42fd5a1ca1cedd1cb
kelleytinavw <kelleytina.williams@my.hamptonu.edu>
2018-11-11 23:27:22 -0500

    aa

--- puyopuyo.java
+++ puyopuyo.java
@@ -1,13 +1,30 @@
 public class puyopuyo{
    public static void main(String [] args){
-      puyopuyoMethods test = new puyopuyoMethods();
-      
-      test.storeNum();
-      for(int i = 0; i < 30; i++){
-        test.printBoard();
+      puyopuyoMethods game = new puyopuyoMethods();
+
+      System.out.println("In PuyoPuyo, try to match as many row values as possible (the ones nearest to the left!) to score points.\nBut be sure not to try and fill a row that already has 10 values! You will lose a turn trying to add to it!\n\n");
+      game.storeNum();
+      //test.moveToBoard();
+      int numTurns = 30;
+      for(int i = 0; i < 30; i++) {
+        System.out.println("Turns left: " + numTurns);
+        numTurns--;
+        game.moveToBoard();
+        if(i > 0)
+          game.score();
+        game.print();
+        System.out.println("\n");
       }
-//       test.score();
-     // for(int i = 0; i < 30; i++){
-//       }  
+
+      System.out.println("Final Score: " + game.getScore());
+      if(game.getScore() >= 30)
+        System.out.println("You're a PuyoPuyo pro!");
+      else if(game.getScore() >= 20 && game.getScore() < 30)
+        System.out.println("You're pretty good.");
+      else if(game.getScore() >= 10 && game.getScore() < 20)
+        System.out.println("You're not too bad.");
+      else
+        System.out.println("Pretty low score, get better.");
+
    }
 }
--- puyopuyoMethods.java
+++ puyopuyoMethods.java
@@ -5,16 +5,21 @@ public class puyopuyoMethods{
    private final int SIZE = 30;
    private final int BOARD_SIZE = 10;
    private int [] stackA = new int [BOARD_SIZE];
+   private int [] temp_stackA = new int[BOARD_SIZE];
    private int [] stackB = new int[BOARD_SIZE];
+   private int [] temp_stackB = new int[BOARD_SIZE];
    private int [] stackC = new int[BOARD_SIZE];
+   private int [] temp_stackC = new int[BOARD_SIZE];
    private int [] queue = new int [SIZE];
    private int stackA_top = 0;
+   private int temp_stackA_top = 0;
    private int stackB_top = 0;
+   private int temp_stackB_top = 0;
    private int stackC_top = 0;
+   private int temp_stackC_top = 0;
    private int frontQ = 0;
    private int rearQ = 0;
    private int score = 0;
-   private String keep = "";
    private Scanner kb = new Scanner(System.in);
 
    public puyopuyoMethods(){}
@@ -22,108 +27,116 @@ public class puyopuyoMethods{
    public void storeNum(){
      Random rand = new Random();
       for(int i = 0; i < SIZE; i++){
-         queue[i] = rand.nextInt(10)+1;
+         queue[i] = rand.nextInt(9) + 1;
          rearQ++;
       }
    }
-   
-   public boolean checkFullA(){
-      boolean isFull = false;
-      if(stackA_top == BOARD_SIZE){
-         System.out.println("Stack A is full choode another stack:");
-         isFull = true;
-      }
-      return isFull;
-   }
-   
-   public boolean checkFullB(){
-      boolean isFull = false;
-      if(stackB_top == BOARD_SIZE){
-         System.out.println("Stack B is full choode another stack:");
-         isFull = true;
-      }
-      return isFull;
-   }
 
-   public boolean checkFullC(){
-      boolean isFull = false;
-      if(stackC_top == BOARD_SIZE){
-         System.out.println("Stack C is full choode another stack:");
-         isFull = true;
-      }
-      return isFull;
-   }
-   
-   public void moveToBoard(String x){
-     switch(x){
-       case "A": 
-                 if(checkFullA() != false){
-                     whichColumn();
-                     break;
-                 }
+   public void moveToBoard(){
+     switch(whichColumn().toUpperCase()) {
+       case "A": if(stackA_top < 10) {
                  stackA[stackA_top] = queue[frontQ];
+                 //System.out.println(stackA[stackA_top]);
                  frontQ++;
                  stackA_top++;
+               }
                  break;
-       case "B": 
-                  if(checkFullB() != false){
-                     whichColumn();
-                     break;
-                 }
+
+       case "B": if(stackB_top < 10) {
                  stackB[stackB_top] = queue[frontQ];
+                 //System.out.println(stackB[stackB_top]);
                  frontQ++;
                  stackB_top++;
+               }
                  break;
-       case "C": 
-                 if(checkFullC() != false){
-                     whichColumn();
-                     break;
-                 }
-                 stackB[stackC_top] = queue[frontQ];
+
+       case "C":
+                 if(stackC_top < 10) {
+                 stackC[stackC_top] = queue[frontQ];
+                 //System.out.println(stackC[stackC_top]);
                  frontQ++;
                  stackC_top++;
+               }
                  break;
+
+       default: System.out.println("Invalid");
      }
    }
 
-   public void whichColumn(){
-     String n = "";
+   public String whichColumn() {
      System.out.println("The number " + queue[frontQ] + " is falling! What column will it land in? ('A', 'B', 'C')");
-     n = kb.next();    
-     if(!((n.equalsIgnoreCase("A")) || (n.equalsIgnoreCase("B")) || (n.equalsIgnoreCase("C")))){
+     String n = "";
+     n = kb.nextLine();
+     if(n.equalsIgnoreCase("A") || n.equalsIgnoreCase("B") || n.equalsIgnoreCase("C"))
+      return n;
+     else {
        System.out.println("Invalid input, try again.");
-       whichColumn();
+       return whichColumn();
      }
-     frontQ++;
-     moveToBoard(n);
-     keep = n;
    }
 
-//   public void score() {
-//     if(stackA[stackA_top-1] == stackB[stackA_top-1] && stackC[stackA_top-1] == stackB[stackA_top-1]){
-//       score+=10;
-//     }else if(stackA[stackA_top-1] == stackB[stackA_top-1] || stackC[stackC_top-1] == stackB[stackC_top-1]){
-//       score+= 5;
-//     }
-//     System.out.println("The score is: " + score);
-//   }
-  
-  public void printBoard(){
-     if(keep.equalsIgnoreCase("A")){
-         System.out.println(stackA[stackA_top] + " " + stackB[stackA_top] + " " + stackC[stackA_top]);
-         stackA_top--;
-         whichColumn();
-     }else if(keep.equalsIgnoreCase("B")){
-         System.out.println(stackA[stackB_top] + " " + stackB[stackB_top] + " " + stackC[stackB_top]);
-         stackB_top--;
-         whichColumn();
-     }else if(keep.equalsIgnoreCase("C")){
-         System.out.println(stackA[stackC_top] + " " + stackB[stackC_top] + " " + stackC[stackC_top]);
-         stackC_top--;
-         whichColumn();
-     }else{
-         System.out.println(stackA[stackA_top] + " " + stackB[stackB_top] + " " + stackC[stackC_top]);
-     }
-  } 
-  
+  public void score() {
+    if(stackA_top > 0 && stackB_top > 0 && stackC_top > 0) {
+    if((stackA[stackA_top - 1] == stackB[stackB_top - 1] && stackC[stackC_top - 1] == stackB[stackB_top - 1]) && (stackA[stackA_top - 1] != 0 && stackC[stackC_top - 1] != 0 && stackB[stackB_top - 1] !=0)) {
+      score+=10;
+      System.out.println("All three rows match! +10");
+    }
+    else if((stackA[stackA_top - 1] == stackB[stackB_top - 1] || stackC[stackC_top - 1] == stackB[stackB_top - 1]) && (stackA[stackA_top - 1] != 0 && stackC[stackC_top - 1] != 0 && stackB[stackB_top - 1] !=0)) {
+      score+= 5;
+      System.out.println("Two rows match! +5");
+    }
+}
+  }
+
+  public void printStack(int[] perm, int[] temp, int pTop, int tTop, String row) {
+    tTop = 0;
+    System.out.print(row + "\t");
+    for(int i = pTop - 1; i >= 0; i--) {
+      temp[i] = perm[i];
+      pTop--;
+      tTop++;
+      }
+      //System.out.println(temp_stackA[temp_stackA_top - 1]);
+      for(int j = tTop - 1; j >= 0; j--) {
+         System.out.print(temp[j] + "\t");
+         perm[j] = temp[j];
+         tTop--;
+         pTop++;
+      }
+      System.out.println();
+
+
+/*
+      if(stackB_top >= 0) {
+        System.out.print(stackA[i] + "\t");
+        temp_stackB[i] = stackB[i];
+        stackB_top--;
+        temp_stackB_top++;
+      }
+
+      if(stackC_top >= 0) {
+        System.out.println(stackC[i]);
+        temp_stackC[i] = stackC[i];
+        stackC_top--;
+        temp_stackC_top++;
+      }
+
+      System.out.println("Current Score: " + score);
+    }
+    */
+  }
+
+public void print() {
+  printStack(stackA, temp_stackA, stackA_top, temp_stackA_top, "A");
+  printStack(stackB, temp_stackB, stackB_top, temp_stackB_top, "B");
+  printStack(stackC, temp_stackC, stackC_top, temp_stackC_top, "C");
+  System.out.println("Current Score: " + score);
+
+
+}
+
+public int getScore() {
+  return score;
+}
+
 }
